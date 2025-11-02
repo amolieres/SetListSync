36e2584711a74c710a441b9afb57b3759bc260aa
Antonin Molieres <molieres.antonin@gmail.com>
2025-11-02 18:51:29 +0100

    chore: fix some forgotten serializable on model

--- composeApp/src/commonMain/kotlin/com/amolieres/setlistync/core/domain/band/model/Gig.kt
+++ composeApp/src/commonMain/kotlin/com/amolieres/setlistync/core/domain/band/model/Gig.kt
@@ -1,8 +1,10 @@
 package com.amolieres.setlistync.core.domain.band.model
 
+import kotlinx.serialization.Serializable
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
+@Serializable
 data class Gig @OptIn(ExperimentalTime::class) constructor(
     val id: String,
     val bandId: String,
